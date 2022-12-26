5fa0d1046e95acfbb1a97adb13163b5982963286
jongjin <jongjin024@gmail.com>
2022-12-26 21:08:05 +0900

    add: addvideo

---
+++ src/assets/svg/AddVideo.tsx
@@ -0,0 +1,21 @@
+export const AddVideo = () => {
+  return (
+    <svg
+      width="23"
+      height="23"
+      viewBox="0 0 23 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="9.77496" width="3.45" height="23" rx="1.725" fill="#80BCFF" />
+      <rect
+        y="13.2251"
+        width="3.45"
+        height="23"
+        rx="1.725"
+        transform="rotate(-90 0 13.2251)"
+        fill="#80BCFF"
+      />
+    </svg>
+  );
+};
