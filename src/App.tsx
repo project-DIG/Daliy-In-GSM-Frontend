ed34914e22ec9ec5d83d7eb602aab35153935a18
minjekang <kangminje777@gmail.com>
2022-12-26 22:46:56 +0900

    add: 로그인페이지 라우터

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Layout from 'components/Layout';
 import { Main } from 'pages';
+import Signin from 'pages/Signin';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from 'shared/globalStyles';
 
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Main />} />
+            <Route path="/signin" element={<Signin />} />
           </Routes>
         </BrowserRouter>
       </Layout>
