f1f109babe210f852ccd00045449c5df4af42f8b
jongjin <jongjin024@gmail.com>
2022-12-27 15:04:44 +0900

    add: 영상이 없을 때 UI

--- src/components/UserPage/Video/index.tsx
+++ src/components/UserPage/Video/index.tsx
@@ -4,33 +4,51 @@ import * as I from 'assets/svg';
 
 function VideoSection() {
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [isMine, setIsMine] = useState<boolean>(false);
+  const [listLength] = useState<number>(0);
 
   return (
     <>
-      <S.Category>
-        <S.Box
-          onClick={() => setActiveIndex(0)}
-          className={activeIndex === 0 ? 'active user' : ''}
-        >
-          <I.User />
-          <S.BottomLine />
-        </S.Box>
-        <S.Box
-          onClick={() => setActiveIndex(1)}
-          className={activeIndex === 1 ? 'active like' : ''}
-        >
-          <I.Like />
-          <S.BottomLine />
-        </S.Box>
-        <S.Box
-          onClick={() => setActiveIndex(2)}
-          className={activeIndex === 2 ? 'active dislike' : ''}
-        >
-          <I.DisLike />
-          <S.BottomLine />
-        </S.Box>
+      <S.Category isMine={isMine}>
+        {isMine ? (
+          <>
+            <S.Box
+              onClick={() => setActiveIndex(0)}
+              className={activeIndex === 0 ? 'active user' : ''}
+            >
+              <I.User />
+              <S.BottomLine />
+            </S.Box>
+            <S.Box
+              onClick={() => setActiveIndex(1)}
+              className={activeIndex === 1 ? 'active like' : ''}
+            >
+              <I.Like />
+              <S.BottomLine />
+            </S.Box>
+            <S.Box
+              onClick={() => setActiveIndex(2)}
+              className={activeIndex === 2 ? 'active dislike' : ''}
+            >
+              <I.DisLike />
+              <S.BottomLine />
+            </S.Box>
+          </>
+        ) : (
+          <S.Box
+            onClick={() => setActiveIndex(0)}
+            className={activeIndex === 0 ? 'active user' : ''}
+          >
+            <I.User />
+            <S.BottomLine />
+          </S.Box>
+        )}
       </S.Category>
-      <S.VideoSection />
+      {listLength === 0 ? (
+        <S.VideoNotFound>아직 영상이 없어요 😯</S.VideoNotFound>
+      ) : (
+        <S.VideoSection />
+      )}
     </>
   );
 }
