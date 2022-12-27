63940d8f3dee757dbfc27363c806e5107d18da3b
minjekang <kangminje777@gmail.com>
2022-12-27 18:17:27 +0900

    add: 회원가입 폼

--- src/components/SignupPage/index.tsx
+++ src/components/SignupPage/index.tsx
@@ -1,6 +1,7 @@
 import * as S from './style';
 import * as I from '../../assets/svg';
 import { Link } from 'react-router-dom';
+import Input from 'components/Common/Input';
 export default function SignupPage() {
   return (
     <S.SignupLayout>
@@ -13,6 +14,17 @@ export default function SignupPage() {
           <br />
           Start!
         </S.LetsStart>
+        <span>
+          <Input type="text" placeholder="이름" />
+          <Input type="password" placeholder="비밀번호" />
+          <Input type="password" placeholder="비밀번호 확인" />
+          <S.EmailText>@gsm.hs.kr</S.EmailText>
+          <Input />
+          <S.AuthenticationBox>
+            <S.Input type="text" placeholder="인증번호" />
+            <S.Check>확인</S.Check>
+          </S.AuthenticationBox>
+        </span>
       </S.SignupSection>
     </S.SignupLayout>
   );
--- src/components/SignupPage/style.ts
+++ src/components/SignupPage/style.ts
@@ -19,5 +19,45 @@ export const LetsStart = styled.div`
   line-height: 53px;
   color: #6c8ee5;
   margin-top: 64px;
-  margin-bottom: 30px;
+  margin-bottom: 60px;
+`;
+
+export const EmailText = styled.p`
+  margin-top: 20px;
+  left: 156px;
+  font-size: 18px;
+  color: #7e7e7e;
+  position: absolute;
+`;
+export const Input = styled.input`
+  width: 126px;
+  height: 60px;
+  border: 2px solid #9fb6ee;
+  border-radius: 16px;
+  font-size: 16px;
+  padding: 0 20px;
+  margin-bottom: 20px;
+  font-family: 'IBM Plex Sans KR';
+  font-style: normal;
+  font-weight: 400;
+  &:focus {
+    border-color: #375dbb;
+    outline: #375dbb;
+  }
+`;
+
+export const Check = styled.button`
+  cursor: pointer;
+  width: 170px;
+  height: 64px;
+  border-radius: 16px;
+  background-color: #9fb6ee;
+  outline: 2px Solid #7e9ce8;
+  border: none;
+`;
+
+export const AuthenticationBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
