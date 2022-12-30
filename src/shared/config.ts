9c55e3f6cd9fc1d0bb04207215cee5571fc30cfa
jongjin <jongjin024@gmail.com>
2022-12-31 00:09:22 +0900

    update: base_url

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = 'http://10.82.20.136:8000/';
+export const BASE_URL = {
+  ABOUT_VIDEO:
+    'http://dig-django-main-alb-397837959.ap-northeast-2.elb.amazonaws.com/api/',
+  ABOUT_USER: 'http://3.39.255.127:8001/',
+};
