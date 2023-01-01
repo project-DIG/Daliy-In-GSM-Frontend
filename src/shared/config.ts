c21fe4cd41f912f1430d2296db28e11e434fd220
jongjin <jongjin024@gmail.com>
2023-01-02 01:27:23 +0900

    fix: url

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = {
   ABOUT_VIDEO:
-    'http://dig-django-main-alb-397837959.ap-northeast-2.elb.amazonaws.com/api/',
+    'http://dig-django-main-alb-397837959.ap-northeast-2.elb.amazonaws.com/',
   ABOUT_USER: 'http://3.39.255.127:8001/',
 };
