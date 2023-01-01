ee5b6a1f207e30fbbed082f886dde4a5dca857de
jongjin <jongjin024@gmail.com>
2023-01-02 05:06:32 +0900

    add: upload button

--- src/components/Modal/CreateVideo/index.tsx
+++ src/components/Modal/CreateVideo/index.tsx
@@ -1,18 +1,40 @@
 import { createVideoModalAtom } from 'atoms';
 import ModalLayout from 'components/Layout/Modal';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import * as S from './style';
 import * as I from 'assets/svg';
+import FollowButton from 'components/Common/FollowButton';
 
 function CreateVideoModal() {
   const [, setCreateVideoModal] = useRecoilState(createVideoModalAtom);
+  const [url, setUrl] = useState<string>('');
+  const [fileIsIn, setFileIsIn] = useState<boolean>(false);
+
+  const setVideo = useRef<any>(null);
+
+  const imgBtnClick = (e: any) => {
+    e.preventDefault();
+    setVideo.current?.click();
+  };
+
+  const handleChange = (e: any) => {
+    setFileIsIn(true);
+    setUrl(URL.createObjectURL(e.target.files[0]));
+    const formData = new FormData();
+    formData.append('image', e.target.files[0]);
+  };
+
+  const removeFile = () => {
+    setUrl('');
+    setFileIsIn(false);
+  };
 
   return (
     <ModalLayout setModal={setCreateVideoModal}>
       <S.Modal onClick={e => e.stopPropagation()}>
         <S.Topbar>
-          <S.Text>업로드</S.Text>
+          <FollowButton className="upload-btn">업로드</FollowButton>
           <div onClick={() => setCreateVideoModal(false)}>
             <I.ExitModal />
           </div>
@@ -20,9 +42,28 @@ function CreateVideoModal() {
         <S.InputBox>
           <S.Title placeholder="제목을 입력하세요" />
         </S.InputBox>
-        <S.UploadBox>
-          <I.UploadButton />
-        </S.UploadBox>
+
+        <S.UploadVideoBox>
+          <div onClick={imgBtnClick}>{!url && <I.UploadButton />}</div>
+          <input
+            ref={setVideo}
+            type={'file'}
+            id={'profile'}
+            accept={'video/*'}
+            name={'file'}
+            onChange={handleChange}
+          />
+          <S.Video fileIsIn={fileIsIn}>
+            {url && (
+              <>
+                <video src={url} controls width="100%" height="100%" />
+                <S.RemoveFile onClick={removeFile}>
+                  <I.ExitModal />
+                </S.RemoveFile>
+              </>
+            )}
+          </S.Video>
+        </S.UploadVideoBox>
       </S.Modal>
     </ModalLayout>
   );
--- src/components/Modal/CreateVideo/style.ts
+++ src/components/Modal/CreateVideo/style.ts
@@ -18,21 +18,18 @@ export const Topbar = styled.div`
   & > div > svg {
     cursor: pointer;
   }
-`;
 
-export const Text = styled.p`
-  font-family: 'Karla';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  color: #000000;
+  .upload-btn {
+    width: 60px;
+    margin: 0;
+  }
 `;
 
 export const InputBox = styled.div`
   width: 86%;
   height: 42px;
   text-align: center;
-  margin: 6rem auto 0;
+  margin: 4rem auto 0;
   box-sizing: border-box;
   border-bottom: 1px solid black;
 `;
@@ -47,14 +44,41 @@ export const Title = styled.input`
   font-size: 30px;
 `;
 
-export const UploadBox = styled.div`
+export const UploadVideoBox = styled.div`
   width: 100%;
-  height: calc(80vh - (42px + 6rem + 3rem + 35px + 4rem));
+  height: 100px;
   margin-top: 4rem;
+  height: calc(80vh - (77px + 11rem));
   border-radius: 80px 80px 0 0;
   box-shadow: 0 -20px 0 #d8e6f3;
   text-align: center;
-  display: flex;
   align-items: center;
   justify-content: center;
+  display: flex;
+
+  input {
+    display: none;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
+export const Video = styled.div<{ fileIsIn: boolean }>`
+  position: relative;
+  display: ${e => (e.fileIsIn ? '' : 'none')};
+  width: 90%;
+  height: 90%;
+
+  video {
+    border-radius: 10px;
+  }
+`;
+
+export const RemoveFile = styled.div`
+  position: absolute;
+  top: 3%;
+  right: 5%;
+  z-index: 100;
 `;
