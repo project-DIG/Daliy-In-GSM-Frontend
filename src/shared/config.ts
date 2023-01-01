bd4f8282dd67f50cf46ae657730526993b7f4163
jongjin <jongjin024@gmail.com>
2023-01-02 01:41:07 +0900

    update: base_url

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,4 @@
 export const BASE_URL = {
-  ABOUT_VIDEO:
-    'http://dig-django-main-alb-397837959.ap-northeast-2.elb.amazonaws.com/',
-  ABOUT_USER: 'http://3.39.255.127:8001/',
+  ABOUT_USER: 'https://user.dailyingsm.shop/',
+  ABOUT_VIDEO: 'https://server.dailyingsm.shop/',
 };
