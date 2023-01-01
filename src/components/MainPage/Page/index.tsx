670c6a849d0e1f10a6c2c10b3640055cd8d03f9c
minjekang <kangminje777@gmail.com>
2023-01-02 01:42:24 +0900

    add: 한페이지씩 스크롤

--- src/components/MainPage/Page/index.tsx
+++ src/components/MainPage/Page/index.tsx
@@ -2,53 +2,42 @@ import * as S from './style';
 import VideoBottomBar from '../Videos/BottomBar';
 import Video from '../Videos/Video';
 import VideoSideBar from '../Videos/SideBar';
-import { useEffect, useState } from 'react';
-import { FullPage, Slide } from 'react-full-page';
+
 function MainPage() {
   return (
     <S.MainPageLayout>
-      <FullPage>
-        <Slide>
-          <S.MainBg>
-            <S.VideoSection>
-              <Video />
-              <VideoSideBar />
-              <VideoBottomBar />
-            </S.VideoSection>
-            <S.Line />
-          </S.MainBg>
-        </Slide>
-        <Slide>
-          <S.MainBg>
-            <S.VideoSection>
-              <Video />
-              <VideoSideBar />
-              <VideoBottomBar />
-            </S.VideoSection>
-            <S.Line />
-          </S.MainBg>
-        </Slide>
-        <Slide>
-          <S.MainBg>
-            <S.VideoSection>
-              <Video />
-              <VideoSideBar />
-              <VideoBottomBar />
-            </S.VideoSection>
-            <S.Line />
-          </S.MainBg>
-        </Slide>
-        <Slide>
-          <S.MainBg>
-            <S.VideoSection>
-              <Video />
-              <VideoSideBar />
-              <VideoBottomBar />
-            </S.VideoSection>
-            <S.Line />
-          </S.MainBg>
-        </Slide>
-      </FullPage>
+      <S.MainBg>
+        <S.VideoSection>
+          <Video />
+          <VideoSideBar />
+          <VideoBottomBar />
+        </S.VideoSection>
+        <S.Line />
+      </S.MainBg>
+      <S.MainBg>
+        <S.VideoSection>
+          <Video />
+          <VideoSideBar />
+          <VideoBottomBar />
+        </S.VideoSection>
+        <S.Line />
+      </S.MainBg>
+      <S.MainBg>
+        <S.VideoSection>
+          <Video />
+          <VideoSideBar />
+          <VideoBottomBar />
+        </S.VideoSection>
+        <S.Line />
+      </S.MainBg>
+      <S.MainBg>
+        <S.VideoSection>
+          <Video />
+          <VideoSideBar />
+          <VideoBottomBar />
+        </S.VideoSection>
+        <S.Line />
+      </S.MainBg>
     </S.MainPageLayout>
   );
 }
--- src/components/MainPage/Page/style.ts
+++ src/components/MainPage/Page/style.ts
@@ -3,7 +3,13 @@ import styled from '@emotion/styled';
 export const MainPageLayout = styled.div`
   background-color: white;
   width: 100%;
+  height: 100vh;
   grid-template-columns: repeat(1, 1fr);
+  overflow: auto;
+  scroll-snap-type: y mandatory;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const VideoSection = styled.section`
@@ -23,6 +29,7 @@ export const Line = styled.div`
 
 export const MainBg = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-color: white;
+  scroll-snap-align: start;
 `;
